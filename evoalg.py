648c0ef48352938d156c49a43e16755c8b2a9552
unknown <jonassen.didrik@gmail.com>
2013-02-12 19:51:01 +0100

    Start of loop. Missing starting population

--- evoalg.py
+++ evoalg.py
@@ -9,3 +9,10 @@ def main(phenotype_class):
 	goal = float(input("Input fitness goal: "))
 	protocol = adult_selection.protocols[raw_input("Input the wanted selection protocol (" + "/".join(adult_selection.protocols.keys() + "): ")](popsize)
 	mechanism = selection_mechanism.mechanisms[raw_input("Input the wanted selection mechanism (" + "/".join(selection_mechanisms.mechanisms.keys() + "): ")](protocol.litter_size)
+	
+	best = []
+	average = []
+	std_dev = []
+	
+	generation = 0
+	while generation < generations and goal != 0 and best[-1] < goal:
