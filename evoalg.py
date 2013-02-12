b14af0c7b1424198ba014c840a19fb0445e6e198
unknown <jonassen.didrik@gmail.com>
2013-02-12 22:51:34 +0100

    added option to goal in evoalg

--- evoalg.py
+++ evoalg.py
@@ -7,7 +7,7 @@ def main(phenotype_class):
 	popsize = int(input("Input population size: "))
 	babymaker = production.production(phenotype_class)
 	generations = int(input("Input number of generations: "))
-	goal = float(input("Input fitness goal: "))
+	goal = float(input("Input fitness goal, 0 if not applicable: "))
 	protocol = adult_selection.protocols[raw_input("Input the wanted selection protocol (" + "/".join(adult_selection.protocols.keys()) + "): ")](popsize)
 	mechanism = selection_mechanisms.mechanisms[raw_input("Input the wanted selection mechanism (" + "/".join(selection_mechanisms.mechanisms.keys()) + "): ")](protocol.litter_size)
 
