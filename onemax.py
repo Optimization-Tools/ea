102a83fe55737dc006ebb7c5f0596f2f419daa8e
unknown <jonassen.didrik@gmail.com>
2013-02-12 21:07:36 +0100

    args be gone

--- onemax.py
+++ onemax.py
@@ -6,7 +6,7 @@ class onemax_ptype(binary_gtype.binary_genotype):
 	def fitness(self):
 		return sum(self.gene)/len(self.gene)
 
-	def __init__(self, args):
+	def __init__(self):
 		super(onemax_ptype, self).__init__(problem_size)
 
 problem_size = int(input("Input problem size: "))
