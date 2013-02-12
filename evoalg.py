dd9ed1aceaa27a43f36455611fcad474aac5b682
unknown <jonassen.didrik@gmail.com>
2013-02-12 19:21:44 +0100

    Continued on setup in evoalg.py

--- evoalg.py
+++ evoalg.py
@@ -1,9 +1,11 @@
 import production
 import adult_selection
+import selecton_mechanism
 
 def main(phenotype_class):
 	popsize = int(input("Input population size: "))
 	babymaker = production.production(phenotype_class)
 	generations = int(input("Input number of generations: "))
 	goal = float(input("Input fitness goal: "))
-	protocol = adult_selection.protocols[input()](popsize)
+	protocol = adult_selection.protocols[raw_input("Input the wanted selection protocol (" + "/".join(adult_selection.protocols.keys() + "): ")](popsize)
+	mechanism = selection_mechanism.mechanisms[raw_input("Input the wanted selection mechanism (" + "/".join(selection_mechanisms.mechanisms.keys() + "): ")](protocol.litter_size)
