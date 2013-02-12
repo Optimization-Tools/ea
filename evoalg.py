3eea4554641a1eb2dc2cfcb6640832af091a6daf
Imre Kerr <imre.kerr@gmail.com>
2013-02-12 19:23:18 +0100

    Revert "førstemann til å commite, jeg vant"
    This reverts commit c1384693f330fd2dbb232f00dcec249d2e171ba9.

--- evoalg.py
+++ evoalg.py
@@ -6,4 +6,4 @@ def main(phenotype_class):
 	babymaker = production.production(phenotype_class)
 	generations = int(input("Input number of generations: "))
 	goal = float(input("Input fitness goal: "))
-	protocol = adult_selection.protocols[raw_input("Choose selection protocol (" + "/".join(adult_selection.protocols.keys()) + "): ")](popsize)
+	protocol = adult_selection.protocols[input()](popsize)
