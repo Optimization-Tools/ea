31c827dc6e274a0b33cf007c4a9c1d2851802074
Imre Kerr <imre.kerr@gmail.com>
2013-02-13 17:11:32 +0100

    Fitness testing is now done on entire population

--- population.py
+++ population.py
@@ -5,10 +5,8 @@ class population:
 		
 	def __init__(this, individuals):
 		this.individuals = individuals
-		
-	def set_individuals(this, individuals):
-		this.individuals = individuals
-		
+		this.individuals[0].calc_fitness(individuals)
+
 	def get_individuals(this):
 		return this.individuals
 		
@@ -16,18 +14,18 @@ class population:
 		return len(this.individuals)
 		   
 	def get_most_fit(this, number):
-		this.individuals.sort(key=lambda pheno: (pheno.fitness()), reverse=True) # Sort individuals from most to least fit
+		this.individuals.sort(key=lambda pheno: (pheno.fitness), reverse=True) # Sort individuals from most to least fit
 		return population(this.individuals[0:min(number, len(this.individuals))])
 		
 	def combine(this, that):
 		return population(this.individuals + that.individuals)
 		
 	def average_fitness(this):
-		return sum([pheno.fitness() for pheno in this.individuals])/this.size()
+		return sum([pheno.fitness for pheno in this.individuals])/this.size()
 		
 	def max_fitness(this):
-		return max([pheno.fitness() for pheno in this.individuals])
+		return max([pheno.fitness for pheno in this.individuals])
 
 	def fitness_standard_deviation(this):
 		average = this.average_fitness()
-		return (sum([(pheno.fitness() - average) ** 2 for pheno in this.individuals])/this.size()) ** 0.5
+		return (sum([(pheno.fitness - average) ** 2 for pheno in this.individuals])/this.size()) ** 0.5
