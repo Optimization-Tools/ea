fdb2c06fbb1ededc980b0b60a56e92c503652965
Didrik Jonassen <jonassen.didrik@gmail.com>
2013-02-13 20:17:44 +0100

    Battle in blotto.py implemented

--- blotto.py
+++ blotto.py
@@ -8,10 +8,27 @@ class blotto_ptype(binary_gtype.binary_genotype):
 	morale = 1.
 	
 	def battle(this, that):
-		
+		for i in xrange(problem_size):
+			diff = this.phenotype[i] - that.phenotype[i]
+			if diff > 0:
+				this.fitness += 2
+				that.morale *= (1-lf)
+				if i < problem_size-1
+					distribute = diff*rf/(problem_size-i+1)
+					for battle in this.phenotype[i+1:]:
+						battle += distribute
+			elif diff < 0:
+				that.fitness += 2
+				this.morale *= (1-lf)
+				if i < problem_size-1:
+					distribute = -diff*rf/(problem_size-i+1)
+					for battle in that.phenotype[i+1:]:
+						battle += distribute
+			else:
+				this.fitness += 1
+				that.fitness += 1
 	
 	def calc_fitness(this, lords):
-		#TODO: Should make it so that the fitness function takes in a full population instead of just one phenotype, then updates a fitness-variable for every phenotype.
 		for i in range(len(lords)):
 			individuals[i].battle(lord) for lord in lords[i+1:]
 		
