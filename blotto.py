5840dffd913d331034fbe2d25b1e3816e42388d2
Didrik Jonassen <jonassen.didrik@gmail.com>
2013-02-13 19:21:07 +0100

    More changes to blotto.py

--- blotto.py
+++ blotto.py
@@ -4,12 +4,21 @@ import evoalg
 
 class blotto_ptype(binary_gtype.binary_genotype):
 	phenotype = None
+	fitness = 0
+	morale = 1.
 	
-	def fitness(this):
+	def battle(this, that):
+		
+	
+	def calc_fitness(this, lords):
 		#TODO: Should make it so that the fitness function takes in a full population instead of just one phenotype, then updates a fitness-variable for every phenotype.
+		for i in range(len(lords)):
+			individuals[i].battle(lord) for lord in lords[i+1:]
 		
 	def generate_phenotype(this):
 		this.phenotype = gene.develop_intlist(4)
+		total = sum(this.phenotype)
+		phenotype = [battle/total for battle in phenotype]
 
 	def __init__(this, gene=None):
 		super(blotto_ptype, this).__init__(gene)
